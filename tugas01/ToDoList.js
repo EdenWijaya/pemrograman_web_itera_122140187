4f8e6c711889dce602abbad96c6fa613e56c4682
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-03-20 12:52:10 +0700

    add button and change file name

--- tugas01/ToDoList.js
+++ tugas01/ToDoList.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnTambahTodo = document.getElementById("btn-tambah-todo");
     const listTodo = document.getElementById("list-todo");
 
-    // Load data dari localStorage saat halaman dimuat
     loadTodos();
 
     btnTambahTodo.addEventListener("click", function () {
@@ -17,19 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function tambahTodo(text) {
         const li = document.createElement("li");
-        li.className = "flex justify-between items-center bg-gray-100 p-2 my-2 rounded";
+        li.className = "flex justify-between items-center bg-gray-100 p-3 my-2 rounded";
 
         const span = document.createElement("span");
         span.textContent = text;
-        span.className = "flex-1 cursor-pointer";
-        span.addEventListener("click", function () {
-            li.classList.toggle("line-through");
-            simpanTodos();
-        });
+        span.className = "flex-1 ";
 
         const btnHapus = document.createElement("button");
         btnHapus.textContent = "Hapus";
-        btnHapus.className = "bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600";
+        btnHapus.className = "bg-red-800 text-white px-2 py-1 rounded cursor-pointer hover:bg-red-600";
         btnHapus.addEventListener("click", function () {
             li.remove();
             simpanTodos();
@@ -37,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const btnSelesai = document.createElement("button");
         btnSelesai.textContent = "Selesai";
-        btnSelesai.className = "bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 ml-2";
+        btnSelesai.className = "bg-green-800 text-white px-2 py-1 rounded cursor-pointer hover:bg-green-600 ml-2";
         btnSelesai.addEventListener("click", function () {
             li.classList.toggle("line-through");
             simpanTodos();
