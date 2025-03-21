f22adf66d5c20c45b6e0c9f01743667fcbcfe833
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-03-22 00:10:32 +0700

    add nomor 3

---
+++ per01/tugas01/nomor3.js
@@ -0,0 +1,28 @@
+document.getElementById("validasiForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nama = document.getElementById("nama").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    let pesanError = "";
+
+    if (nama.length <= 3) {
+        pesanError = "Nama harus lebih dari 3 karakter \n";
+    }
+
+    if (!email.includes("@" && ".com")) {
+        pesanError = "Email harus valid\n";
+    }
+
+    if (password.length < 8) {
+        pesanError = "Password harus minimal 8 karakter \n";
+    }
+
+    if (pesanError) {
+        alert(pesanError); 
+    } else {
+        alert("Form berhasil dikirim!");
+        this.submit(); 
+    }
+});
