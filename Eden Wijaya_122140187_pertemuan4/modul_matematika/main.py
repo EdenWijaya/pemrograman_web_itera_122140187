8284b9f241422276cac19b163242088869e3dfe4
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-04-28 00:05:44 +0700

    update no 3

--- Eden Wijaya_122140187_pertemuan4/modul_matematika/main.py
+++ Eden Wijaya_122140187_pertemuan4/modul_matematika/main.py
@@ -2,22 +2,20 @@ import math_operations
 from math_operations import celsius_to_fahrenheit, celsius_to_kelvin
 from math_operations import hitung_luas_persegi, hitung_keliling_persegi, hitung_luas_persegi_panjang, hitung_keliling_persegi_panjang, hitung_luas_lingkaran, hitung_keliling_lingkaran
 
-print ("\n--hitung luas dan keliling--")
-s = 5
-p = 10
-lebar = 4
-r = 7
+print("\n=== luas dan keliling ===")
+s = float(input("Masukkan sisi persegi: "))
+p = float(input("Masukkan panjang persegi panjang: "))
+lebar = float(input("Masukkan lebar persegi panjang: "))
+r = float(input("Masukkan jari jari lingkaran: "))
 
-print (f"luas persegi = {hitung_luas_persegi(s)}")
+print (f"\nluas persegi = {hitung_luas_persegi(s)}")
 print (f"keliling persegi = {hitung_keliling_persegi(s)}")
-print (f"\nluas persegi panjang = {hitung_luas_persegi_panjang(p, lebar)}")
+print (f"luas persegi panjang = {hitung_luas_persegi_panjang(p, lebar)}")
 print (f"keliling persegi panjang = {hitung_keliling_persegi_panjang(p, lebar)}")
-print (f"\nluas lingkaran = {hitung_luas_lingkaran(r)}")
+print (f"luas lingkaran = {hitung_luas_lingkaran(r)}")
 print (f"keliling lingkaran = {hitung_keliling_lingkaran(r)}")
 
-print ("\n")
-
-print ("\n--konversi suhu--")
-celsius = 30
-print (f"celsius to fahrenheit = {celsius_to_fahrenheit(celsius)}")
+print("\n=== Konversi Suhu ===")
+celsius = float(input("Masukkan suhu dalam Celsius: "))
+print (f"\ncelsius to fahrenheit = {celsius_to_fahrenheit(celsius)}")
 print (f"celsius to kelvin = {celsius_to_kelvin(celsius)}")
